a35a677f464c97b994e9cdebcf96cd21dd485963
Kevin Roche <kevinroche222@gmail.com>
2021-11-08 22:26:53 -0500

    initial commit of band name generator

---
+++ Project 01 - Band Name Generator/bandNameGenerator.py
@@ -0,0 +1,15 @@
+#######################
+# Band Name Generator #
+#######################
+
+## This program asks users their name. Once the user has given their name,
+## the program returns a random two-word band name to the user in verb-noun format.
+
+## Import packages
+from random_word import RandomWords
+r = RandomWords()
+
+## Generate band name
+print(input("What is your name? ") + ", your band name is " + r.get_random_word(hasDictionaryDef = "true", includePartOfSpeech="verb", minLength = 5, maxLength = 10, minCorpusCount = 5, minDictionaryCount = 5) + " " + r.get_random_word(hasDictionaryDef = "true", includePartOfSpeech="noun", minLength = 5, maxLength = 10, minCorpusCount = 5, minDictionaryCount = 5))
+
+
